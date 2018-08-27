24d8cf4efcb31e4341d30e25bd3a213b68d2524e
Robert Leedy <leedyrj@gmail.com>
2018-08-27 11:25:30 -0500

    finished family dictionary exercise

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace familydictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+
+            myFamily.Add("sister", new Dictionary<string, string>() {
+                {"name", "Mindy"},
+                {"age", "37"},
+            });
+
+            myFamily.Add("wife", new Dictionary<string, string>() {
+                {"name", "Melia"},
+                {"age", "30"},
+            });
+
+            foreach (KeyValuePair<string, Dictionary<string, string>> member in myFamily)
+            {
+                // Console.WriteLine($"memberKey {member.Key}");
+                // foreach (KeyValuePair<string, string> person in member.Value)
+                // {
+                // Console.WriteLine($"personKey {person.Key}");
+                // Console.WriteLine($"personValue {person.Value}");
+                Console.WriteLine($"{member.Value["name"]} is my {member.Key} and is {member.Value["age"]} years old.");
+                // }
+            }
+        }
+    }
+}
