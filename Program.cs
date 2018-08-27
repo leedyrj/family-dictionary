f2ab387e5578b0fcb330c515fe83961889873f7d
Robert Leedy <leedyrj@gmail.com>
2018-08-27 11:27:14 -0500

    finished family dictionary exercise

--- Program.cs
+++ Program.cs
@@ -9,14 +9,24 @@ namespace familydictionary
         {
             Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
 
+            myFamily.Add("wife", new Dictionary<string, string>() {
+                {"name", "Melia"},
+                {"age", "30"},
+            });
+
             myFamily.Add("sister", new Dictionary<string, string>() {
                 {"name", "Mindy"},
                 {"age", "37"},
             });
 
-            myFamily.Add("wife", new Dictionary<string, string>() {
-                {"name", "Melia"},
-                {"age", "30"},
+            myFamily.Add("mom", new Dictionary<string, string>() {
+                {"name", "Colleen"},
+                {"age", "65"},
+            });
+
+            myFamily.Add("dad", new Dictionary<string, string>() {
+                {"name", "Bob"},
+                {"age", "63"},
             });
 
             foreach (KeyValuePair<string, Dictionary<string, string>> member in myFamily)
